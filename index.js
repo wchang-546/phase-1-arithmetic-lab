89243fe522e20e299b4f0945b5f0b64ab628216a
Will Chang <william.chaang@gmail.com>
2023-06-14 11:42:35 -0600

    Arithmetic completed.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = (num1 * num2);
 
-// Write your code here
+const random = Math.ceil(Math.random()*10) + 1;
+
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4; 
+
+const max = Math.max(1, 2, 4, 5, 7, 20);
